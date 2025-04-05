00a8d423180eb67a16bbadd16764e3975f0ded74
Rafae1TCC <rafael.cabrera32@uabc.edu.mx>
2025-04-04 17:16:46 -0700

    Final Report

--- scripts/app.js
+++ scripts/app.js
@@ -11,7 +11,40 @@ function saveTask() {
   let taskToSave = new Task(title, description, color, date, status, budget);
   console.log(taskToSave);
 
-  displayTask(taskToSave);
+  $.ajax({
+    type: "POST",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+    data: JSON.stringify(taskToSave),
+    contentType: "application/json",
+    success: function (response) {
+      console.log(response);
+      displayTask(taskToSave);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
+
+function loadTask() {
+  $.ajax({
+    type: "GET",
+    url: "http://fsdiapi.azurewebsites.net/api/tasks/",
+    success: function (response) {
+      console.log(response);
+      let data = JSON.parse(response);
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        let list = data[i];
+        if (list.name == "Rafael") {
+          displayTask(list);
+        }
+      }
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
 }
 
 function displayTask(task) {
@@ -30,7 +63,7 @@ function displayTask(task) {
 
         <div class="task-date-budget">
           <span>${task.date}</span>
-          <span>${task.budget}</span>
+          <span>$${task.budget}</span>
         </div>
       </div>
     </div>
@@ -41,9 +74,23 @@ function displayTask(task) {
   $("#list").append(syntax);
 }
 
+function testRequest() {
+  $.ajax({
+    type: "get",
+    url: "http://fsdiapi.azurewebsites.net",
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
+
 function init() {
   console.log("Init");
   $("#btnSave").click(saveTask);
+  loadTask();
 }
 
 window.onload = init;
